c6a7e4d37f73357369cf30fcb75f5ab0cb1faa63
Enrique Barca <barcaenrique7@gmail.com>
2025-01-15 23:56:38 +0100

    gh-1 Create project base, gh-2 Add h2 start scripting data, gh-3 Create bussines logic, driving side and driven side

--- src/main/java/com/inditex/pricing/domain/model/Price.java
+++ src/main/java/com/inditex/pricing/domain/model/Price.java
@@ -1,6 +1,9 @@
 package com.inditex.pricing.domain.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 import lombok.Data;
@@ -9,11 +12,17 @@ import lombok.Data;
 @Entity
 @AllArgsConstructor
 public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private int brandId;
     private int productId;
     private int priceList;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
+    private boolean priority;
     private double price;
+    private String curr; //TODO: Make it an enum
 
 }
--- src/main/java/com/inditex/pricing/infrastructure/db/service/GetPriceAdapter.java
+++ src/main/java/com/inditex/pricing/infrastructure/db/service/GetPriceAdapter.java
@@ -5,7 +5,11 @@ import com.inditex.pricing.application.response.PriceResponse;
 import com.inditex.pricing.infrastructure.repo.PriceRepository;
 import com.inditex.pricing.domain.ports.out.GetPricePort;
 import com.inditex.pricing.utils.exception.PriceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Slf4j
+@Service
 public class GetPriceAdapter implements GetPricePort {
 
     private final PriceRepository priceRepository;
@@ -16,17 +20,19 @@ public class GetPriceAdapter implements GetPricePort {
 
     @Override
     public PriceResponse getApplicablePrice(PriceRequest request) throws PriceNotFoundException {
-        return priceRepository
-                .findApplicablePrice(request.getProductId(), request.getBrandId(), request.getApplicationDate())
-                .map(price -> new PriceResponse(
-                        price.getProductId(),
-                        price.getBrandId(),
-                        price.getPriceList(),
-                        price.getStartDate(),
-                        price.getEndDate(),
-                        price.getPrice()
-                ))
-                .orElseThrow(() -> new PriceNotFoundException("Price not found for the given parameters"));
+        System.err.println("getApplicablePrice method response....");
+        return null;
+//        return priceRepository
+//                .findApplicablePrice(request.getProductId(), request.getBrandId(), request.getApplicationDate())
+//                .map(price -> new PriceResponse(
+//                        price.getProductId(),
+//                        price.getBrandId(),
+//                        price.getPriceList(),
+//                        price.getStartDate(),
+//                        price.getEndDate(),
+//                        price.getPrice()
+//                ))
+//                .orElseThrow(() -> new PriceNotFoundException("Price not found for the given parameters"));
     }
 
 }
