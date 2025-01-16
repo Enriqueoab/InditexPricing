061b13abf9d4bc4374dec206de2b6a92538e0e91
Enrique Barca <barcaenrique7@gmail.com>
2025-01-16 17:59:45 +0100

    gh-9 Add security and some controller modification

--- src/main/java/com/inditex/pricing/application/controller/PricingController.java
+++ src/main/java/com/inditex/pricing/application/controller/PricingController.java
@@ -5,14 +5,19 @@ import com.inditex.pricing.application.request.PriceRequest;
 import com.inditex.pricing.application.response.PriceResponse;
 import com.inditex.pricing.utils.exception.DefaultApiResponses;
 import com.inditex.pricing.utils.exception.PriceNotFoundException;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
 @RestController
+@Tag(name = "Price")
+@RequestMapping("/price")
 public class PricingController {
     private final GetPriceUseCase getPriceUseCase;
 
@@ -22,7 +27,9 @@ public class PricingController {
 
     @GetMapping
     @DefaultApiResponses
-    public PriceResponse getPrice(@RequestParam int productId, @RequestParam int brandId, @RequestParam String date) throws PriceNotFoundException {
+    public PriceResponse getPrice(@Valid @RequestParam int productId, @Valid @RequestParam int brandId,
+                                  @Valid @RequestParam String date) throws PriceNotFoundException {
+
         LocalDateTime applicationDate = LocalDateTime.parse(date);
         PriceRequest request = new PriceRequest(productId, brandId, applicationDate);
         return getPriceUseCase.getApplicablePrice(request);
---
+++ src/main/java/com/inditex/pricing/utils/exception/DefaultApiResponses.java
@@ -0,0 +1,15 @@
+package com.inditex.pricing.utils.exception;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+
+@ApiResponse(responseCode = "200", description = "Successful operation")
+@ApiResponse(responseCode = "400", description = "Invalid request")
+@ApiResponse(responseCode = "404", description = "Not found")
+public @interface DefaultApiResponses { }
