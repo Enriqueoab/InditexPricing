4059efaaf153dd640c698a6b645dddacc5bbba7a
Enrique Barca <barcaenrique7@gmail.com>
2025-01-19 23:47:12 +0100

    gh-7 Add Swagger tags

--- src/main/java/com/inditex/pricing/web/request/PriceRequest.java
+++ src/main/java/com/inditex/pricing/web/request/PriceRequest.java
@@ -1,11 +1,17 @@
 package com.inditex.pricing.web.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Price form to request the price for a product and brand in a specific date")
 public record PriceRequest(
 
+        @Schema(description = "Product identifier", example = "35455")
         int productId,
 
+        @Schema(description = "Brand identifier", example = "1")
         int brandId,
 
+        @Schema(description = "Date to check the application price for", example = "2020-06-15T16:30:00")
         String applicationDate
 ) {
 }
--- src/main/java/com/inditex/pricing/web/response/PriceResponse.java
+++ src/main/java/com/inditex/pricing/web/response/PriceResponse.java
@@ -1,16 +1,25 @@
 package com.inditex.pricing.web.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
+@Schema(description = "Price form with the information for the product and brand in a specific date requested")
 public record PriceResponse(
+
+        @Schema(description = "Product identifier", example = "35455")
         int productId,
 
+        @Schema(description = "Brand identifier", example = "1")
         int brandId,
 
+        @Schema(description = "The price will be set from", example = "2020-06-15T16:00:00")
         LocalDateTime startDate,
 
+        @Schema(description = "The price will be set until", example = "2020-12-31T23:59:59")
         LocalDateTime endDate,
 
+        @Schema(description = "The price to be set to", example = "38.95")
         double price
 ) {
 }
